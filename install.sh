583f5b189253c352465e8c907941f4ef17da59c2
hiperwalk <hiperwalk@gmail.com>
2015-05-04 10:16:22 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,9 +12,13 @@ echo "[Hiperwalk installer] Creating /usr/local/hiperwalk."
 mkdir -p /usr/local/hiperwalk
 
 echo "[Hiperwalk installer] Copying new files."
-cp *.py /usr/local/hiperwalk
-cp *.nbl /usr/local/hiperwalk
+mv *.py /usr/local/hiperwalk
+mv *.nbl /usr/local/hiperwalk
+cp LICENSE /usr/local/hiperwalk
+cp README.md /usr/local/hiperwalk
 cp -r examples /usr/local/hiperwalk
+mv ./examples/* .
+rm -rf ./examples
 
 echo "[Hiperwalk installer] Creating dynamic link to /usr/bin/hiperwalk"
 chmod +x /usr/local/hiperwalk/hiperwalk.py 
@@ -22,3 +26,4 @@ chmod +x /usr/local/hiperwalk/hiperwalk.py
 ln -f -s  /usr/local/hiperwalk/hiperwalk.py /usr/bin/hiperwalk
 
 echo "[Hiperwalk installer] Hiperwalk installed at /usr/local/hiperwalk."
+rm -rf install.sh
