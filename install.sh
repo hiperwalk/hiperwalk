6c99df4879f9d05a84cac0adc146a01a4c289c47
pcslara <pedrocslara@gmail.com>
2017-08-04 16:09:44 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,30 +1,33 @@
 #!/bin/bash
 
-if [ "$(id -u)" != "0" ]; then
-   echo "This file must be run as root." 1>&2
-   exit 1
-fi
+#if [ "$(id -u)" != "0" ]; then
+#   echo "This file must be run as root." 1>&2
+#   exit 1
+#fi
 
-rm -rf /usr/local/hiperwalk/*.py
-rm -rf /usr/local/hiperwalk/*.nbl
-rm -rf /usr/local/hiperwalk/hiperwalk
+INST_DIR=$(awk '/INSTALL/{ split($0, array , "="); print array[2]}' config.py | sed 's/"//g;')
 
-echo "[Hiperwalk installer] Creating /usr/local/hiperwalk."
-mkdir -p /usr/local/hiperwalk
+
+rm -rf $INST_DIR/hiperwalk/*.py
+rm -rf $INST_DIR/hiperwalk/*.nbl
+rm -rf $INST_DIR/hiperwalk/hiperwalk
+
+echo "[Hiperwalk installer] Creating $INST_DIR/hiperwalk."
+mkdir -p $INST_DIR/hiperwalk
 
 echo "[Hiperwalk installer] Copying new files."
-mv *.py /usr/local/hiperwalk
-mv *.nbl /usr/local/hiperwalk
-cp LICENSE /usr/local/hiperwalk
-cp README.md /usr/local/hiperwalk
-cp -r examples /usr/local/hiperwalk
+mv *.py $INST_DIR/hiperwalk
+mv *.nbl $INST_DIR/hiperwalk
+cp LICENSE $INST_DIR/hiperwalk
+cp README.md $INST_DIR/hiperwalk
+cp -r examples $INST_DIR/hiperwalk
 mv ./examples/* .
 rm -rf ./examples
 
 echo "[Hiperwalk installer] Creating dynamic link to /usr/bin/hiperwalk"
-chmod +x /usr/local/hiperwalk/hiperwalk.py 
+chmod +x $INST_DIR/hiperwalk/hiperwalk.py 
 
-ln -f -s  /usr/local/hiperwalk/hiperwalk.py /usr/bin/hiperwalk
+ln -f -s  $INST_DIR/hiperwalk/hiperwalk.py /usr/bin/hiperwalk
 
-echo "[Hiperwalk installer] Hiperwalk installed at /usr/local/hiperwalk."
+echo "[Hiperwalk installer] Hiperwalk installed at $INST_DIR/hiperwalk."
 rm -rf install.sh
