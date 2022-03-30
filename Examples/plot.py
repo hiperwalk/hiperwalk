3884d12d6f11088350a98839ff985ff885dac2b1
Gustavowl <gbezerra@posgrad.lncc.br>
2022-03-30 18:25:18 -0300

    Updates plot and old_vs_newFlipFlopShiftOperator examples

--- Examples/plot.py
+++ Examples/plot.py
@@ -5,12 +5,12 @@ from CoinedModel import *
 from PlotModule import *
 from numpy import linspace
 
-num_vert = 12
+num_vert = 5
 
 #G = nx.grid_graph(dim=(num_vert, num_vert), periodic=False)
 #G = nx.complete_graph(num_vert)
 G = nx.circular_ladder_graph(num_vert)
 
-PlotDistributionProbabilityOnGraph(
+PlotProbabilityDistributionOnGraph(
     nx.adjacency_matrix(G), linspace(0, 1, num=G.number_of_nodes())
 )
