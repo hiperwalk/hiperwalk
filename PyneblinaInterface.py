457d43217052e7638ba8a08adbe93b184ca5b865
Gustavowl <gbezerra@posgrad.lncc.br>
2022-03-30 18:22:13 -0300

    Updates PyneblinaInterface to send data to device

--- PyneblinaInterface.py
+++ PyneblinaInterface.py
@@ -1,15 +1,19 @@
 from neblina import *
+from numpy import array as np_array
 
 #Declares data types according to neblina-core
 #TODO: make it so it is not hardcoded
 NEBLINA_FLOAT = 2
 NEBLINA_COMPLEX = 13 
 
-# Transfers a vector (v) to Neblina-core.
+# Transfers a vector (v) to Neblina-core, and moves it to the device to be used.
+# Returns a pointer to this vector (needed to call other pyneblina functions).
 # by default, a vector with complex entries is expected.
 # If the matrix has only real entries, invoke this function by
 # TransferVector(v, False); this saves half the memory that would be used.
-def TransferVector(v, isComplex=True):
+# TODO: is there a way to move the vector to the device directly?
+#   I think an auxiliary vector is beign created, thus twice the memory needed is being used
+def NeblinaSendVector(v, isComplex=True):
     #TODO: check if isComplex automatically?
     n = v.shape[0]
     #TODO: needs better support from pyneblina to use next instruction (commented).
@@ -23,16 +27,41 @@ def TransferVector(v, isComplex=True):
     if isComplex:
         for i in range(n):
             vector_set(vec, i, v[i].real, v[i].imag)
-        return vec
+    else:
+        for i in range(n):
+            #TODO: Pyneblina needs to accept 3 arguments only instead of 4?
+            #TODO: check if vector_set is idetifying the vector type right
+            #(i.e. real and not complex)
+            vector_set(vec, i, v[i], 0)
 
-    # else is real
-    for i in range(n):
-        #TODO: Pyneblina needs to accept 3 arguments only instead of 4?
-        #TODO: check if vector_set is idetifying the vector type right (i.e. real and not complex)
-        vector_set(vec, i, v[i], 0)
+    #suppose that the vector is going to be used immediately after being transferred
+    #TODO: check if this is the case
+    move_vector_device(vec)
     return vec
+
+#Retrieves vector from the device and converts it to python array.
+#By default, it is supposed that the vector is not going to be used in
+#other pyneblina calculations, thus it is going to be deleted to free memory.
+#TODO: get vector dimension(vdim) automatically
+def NeblinaRetrieveVector(v, vdim, deleteVector=True):
+    nbl_vec = move_vector_host(v)
+
+    #TODO: check type automatically, for now suppose it is only complex
+    py_vec = np_array(
+                [vector_get(nbl_vec, 2*i) + 1j*vector_get(nbl_vec, 2*i+1) for i in range(vdim)]
+            )
+
+    #TODO: check if vector is being deleted (or not) according to the demand
+    if deleteVector:
+        print('TODO: vector_delete from pyneblina is not available')
+        #vector_delete(v)
+    print('TODO: vector_delete from pyneblina is not available')
+    #vector_delete(nbl_vec)
+
+    return py_vec
         
-# Transfers a sparse Matrix (M) stored in csr format to Neblina-core.
+# Transfers a sparse Matrix (M) stored in csr format to Neblina-core and
+# moves it to the device (ready to be used).
 # By default, a matrix with complex elements is expected.
 # If the matrix has only real elements, invoke this function by
 # TransferSparseMatrix(M, False); this saves half the memory that would be used.
@@ -41,7 +70,7 @@ def TransferVector(v, isComplex=True):
 #   - Transfer and check complex Matrix
 #TODO: isn't there a way for neblina-core to use the csr matrix directly?
 #   In order to avoid double memory usage
-def TransferSparseMatrix(M, isComplex=True):
+def NeblinaSendSparseMatrix(M, isComplex=True):
     #TODO: check if isComplex automatically?
     n = M.shape[0]
 
@@ -72,5 +101,8 @@ def TransferSparseMatrix(M, isComplex=True):
             #TODO: Pynebliena needs to accept 4 arguments instead of 5?
             #TODO: check if smatrix_set_real_value is beign called instead of smatrix_set_complex_value
             sparse_matrix_set(smat, row, col, M[row, col], 0) 
+
+    sparse_matrix_pack(smat) #TODO: is this needed?
+    move_sparse_matrix_device(smat)
 
     return smat
