b50fbc40764de227c9e8d1884f371550445b5375
Gustavowl <gbezerra@posgrad.lncc.br>
2022-04-04 19:20:09 -0300

    Plots probability of multiple states at once

--- PlotModule.py
+++ PlotModule.py
@@ -7,7 +7,7 @@ from numpy import linspace
 #min_prob and max_prob send separately to give the possibility
 #of min_prob and max_prob of the whole walk (instead of a single step)
 #Expects kwargs['vmin'] = min_prob and kwargs['vmax'] = max_prob
-def ConfigureNodes(G, probabilities, prob_node_size, kwargs):
+def ConfigureNodes(G, probabilities, min_node_size, max_node_size, kwargs):
     #setting colormap related attributes
     if 'cmap' in kwargs:
         if kwargs['cmap'] == 'default':
@@ -27,20 +27,17 @@ def ConfigureNodes(G, probabilities, prob_node_size, kwargs):
     if kwargs['with_labels'] and 'font_color' not in kwargs:
         kwargs['font_color'] = 'black'
 
-    if prob_node_size:
-        if 'node_size' not in kwargs:
-            kwargs['node_size'] = (300, 3000)
-
+    if kwargs['node_size'] is None:
+        if min_node_size is None:
+            min_node_size = 300
+        if max_node_size is None:
+            max_node_size = 3000
+    if min_node_size is not None and max_node_size is not None:
         #calculating size of each node acording to probability 
         #as a function f(x) = ax + b where b = min_size and
         #max_size = a*(max_prob-min_prob) + min_size
-        a = (kwargs['node_size'][1] - kwargs['node_size'][0]) / (kwargs['vmax'] - kwargs['vmin'])
-        b = kwargs['node_size'][0]
-        kwargs['node_size'] = list(map( lambda x: a*x + b, probabilities))
-
-    elif 'node_size' not in kwargs:
-        #standard size times number of largest label's characters
-        kwargs['node_size'] = 300 * len(str(G.number_of_nodes())) 
+        a = (max_node_size - min_node_size) / (kwargs['vmax'] - kwargs['vmin'])
+        kwargs['node_size'] = list(map( lambda x: a*x + min_node_size, probabilities ))
 
     #calculates vertices positions.
     #needed to do beforehand in order to fix position for multiple steps
@@ -53,14 +50,15 @@ def ConfigureNodes(G, probabilities, prob_node_size, kwargs):
 #Proper graph visualization is hard, and we highly recommend that
 #people visualize their graphs with tools dedicated to that task. 
 #https://networkx.org/documentation/stable/reference/drawing.html
+#By default, node sizes are larger if the probability is larger.
+#to fix node size, set "node_size" to a constant or an array, as described in networx documentation.
 #parameters.
-#prob_node_size: Boolean. Node size represents probability.
-#   The larger the node, the larger the probability.
-#   Check node_size kwarg
-#prob_transparency: Boolean. Node transparency represents probability.
-#   The more opaque the noode, the larger the probability.
-#   check alpha kwarg.
-#...
+#min_node_size, max_node_size: node size representing minimum/maximum probability.
+#   default: None. If optional argument node_size is not set,
+#   min_node_size and max_node_size will assume default values (check configure nodes).
+#   If optional argument node_size is set and either min_node_size or max_node_size is None,
+#   all nodes will have the size as described by node_size.
+#   If min_node_size, max_node_size and node_size are set, node_size is disregarded.
 #For detailed info about **kwargs check networkx's documentation for
 #draw_networkx, draw_networkx_nodes, drawnetworkx_edges, etc.
 #Here, a few useful optional keywords are listed
@@ -71,7 +69,8 @@ def ConfigureNodes(G, probabilities, prob_node_size, kwargs):
 #   if ommited and plot_node_size is true, uses default size.
 #alpha: either a float in the [0, 1] interval for fixed node transparency or a float tuple:
 #   (min_alpha, max_alpha). If ommited and plot_transparency is true, uses default values.
-def PlotProbabilityDistributionOnGraph(AdjMatrix, probabilities, prob_node_size=True, **kwargs):
+def PlotProbabilityDistributionOnGraph(AdjMatrix, probabilities, min_node_size=None,
+        max_node_size=None, **kwargs):
 
     #vmin and vmax are default keywords used by networkx_draw.
     #if an invalid keyword is passed to nx.draw(), it does not execute
@@ -80,22 +79,28 @@ def PlotProbabilityDistributionOnGraph(AdjMatrix, probabilities, prob_node_size=
 
     G = nx.from_numpy_matrix(AdjMatrix)
 
-    #TODO: set figure size according to graphdimension
-    fig, ax = ConfigureFigure()
+    if len(probabilities.shape) == 1:
+        probabilities = [probabilities]
+    restoreNodeSize = None if 'node_size' not in kwargs else kwargs['node_size']
 
-    #setting kwargs for plotting
-    #kwargs dict is updated by reference
-    ConfigureNodes(G, probabilities, prob_node_size, kwargs)
-    nx.draw(G, **kwargs)
+    for i in range(len(probabilities)):
+        #TODO: set figure size according to graphdimension
+        fig, ax = ConfigureFigure()
 
-    ##setting colorbar
-    if 'cmap' in kwargs:
-        ConfigureColorbar(ax, kwargs)
+        #setting kwargs for plotting
+        #kwargs dict is updated by reference
+        kwargs['node_size'] = restoreNodeSize
+        ConfigureNodes(G, probabilities[i], min_node_size, max_node_size, kwargs)
+        nx.draw(G, **kwargs)
 
-    #showing img
-    #TODO: add saving option
-    plt.tight_layout()
-    plt.show()
+        ##setting colorbar
+        if 'cmap' in kwargs:
+            ConfigureColorbar(ax, kwargs)
+
+        #showing img
+        #TODO: add saving option
+        plt.tight_layout()
+        plt.show()
 
 #TODO: set figure size according to graphdimension
 def ConfigureFigure(fig_width=10, fig_height=8):
