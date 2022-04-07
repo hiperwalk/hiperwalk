f0d3f584d3182372af0bca71e95a5f7aa7b76f49
Gustavowl <gbezerra@posgrad.lncc.br>
2022-04-06 21:28:33 -0300

    PlotFigure saves files

--- PlotModule.py
+++ PlotModule.py
@@ -60,6 +60,20 @@ def ConfigureNodes(G, probabilities, min_node_size, max_node_size, kwargs):
 #   If optional argument node_size is set and either min_node_size or max_node_size is not set,
 #   all nodes will have the size as described by node_size.
 #   If min_node_size, max_node_size and node_size are set, node_size is disregarded.
+#animate: Boolean. If False, generates one image for each of the probabilities.
+#   If True, generates an animation.
+#show_plot: Boolean. If False, does not show generated plot.
+#   If True, shows the generated plot
+#filename_prefix: str or None, default: None.
+#   If None and show_plot is True, shows the plot and do not save in an output file.
+#   If it is a string, saves plot in an output file;
+#   if animate is True, the animation will be saved in a gif file, e.g. filename_prefix.gif;
+#   if animate is False, saves a .png file for each of the probabilities,
+#   e.g. filename_prefix-1.png, filename_prefix-2.png, etc.
+#   IMPORTANT: we do NOT recommend to use animate=True, show_plot=True and filename_prefix as a str
+#   because the show process will not be optimized (blit = False, check matplotlib documentation);
+#   hence, the time between frames may not be the one expected.
+#
 #For detailed info about **kwargs check networkx's documentation for
 #draw_networkx, draw_networkx_nodes, drawnetworkx_edges, etc.
 #Here, a few useful optional keywords are listed
@@ -70,7 +84,8 @@ def ConfigureNodes(G, probabilities, min_node_size, max_node_size, kwargs):
 #   if ommited and plot_node_size is true, uses default size.
 #alpha: either a float in the [0, 1] interval for fixed node transparency or a float tuple:
 #   (min_alpha, max_alpha). If ommited and plot_transparency is true, uses default values.
-def PlotProbabilityDistributionOnGraph(AdjMatrix, probabilities, animate=False, **kwargs):
+def PlotProbabilityDistributionOnGraph(AdjMatrix, probabilities, animate=False,
+        show_plot=True, filename_prefix=None, **kwargs):
 
     #vmin and vmax are default keywords used by networkx_draw.
     #if an invalid keyword is passed to nx.draw(), it does not execute
@@ -88,18 +103,24 @@ def PlotProbabilityDistributionOnGraph(AdjMatrix, probabilities, animate=False, 
             fig, ax = ConfigureFigure()
             DrawFigure(G, probabilities[i], ax=ax, **kwargs)
 
-            #showing img
-            #TODO: add saving option
-            plt.show()
+            #show or save image (or both)
+            if filename_prefix is not None:
+                plt.savefig(filename_prefix)
+                if not show_plot:
+                    plt.close()
+            if show_plot:
+                plt.show()
 
     else:
         fig, ax = ConfigureFigure()
+        blit = filename_prefix is None #because optimization
         anim  = FuncAnimation(fig, AnimateFigure, frames=probabilities,
-                fargs=(G, ax, kwargs), interval=200, repeat_delay=200, blit=True)
+                fargs=(G, ax, kwargs), interval=200, repeat_delay=200, blit=blit)
 
-        #anim.save('animation_1.gif', writer='imagemagick')
-
-        plt.show()
+        if filename_prefix is not None:
+            anim.save(filename_prefix + '.gif')
+        if show_plot:
+            plt.show()
 
 def AnimateFigure(probabilities, G, ax, kwargs):
     ax.clear()
