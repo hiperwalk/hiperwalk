af5876d0e1fa1310d4ba851b23b588aac55a2378
Koruja <gbezerra@posgrad.lncc.br>
2022-05-04 14:54:08 -0300

    Creates and closes gtk window

--- AnimationModule.py
+++ AnimationModule.py
@@ -13,6 +13,28 @@ def SaveAnimation(anim):
     return None
 
 def ShowAnimation(anim):
-    print('TODO: ShowAnimation')
-    return None
+    from gi.repository import Gtk as gtk
+    from gi.repository.Gdk import KEY_q
+    from gi.repository.Gdk import KEY_Q
+    
+    #creating window
+    window = gtk.Window(title="Animation")
+    window.set_border_width(20)
+    window.set_default_size(400, 200)
 
+    #assign closing events
+    def on_key_press(self, event):
+        if event.keyval == KEY_q or event.keyval == KEY_Q :
+            window.close()
+
+    window.connect("key-press-event", on_key_press)
+    window.connect("destroy", gtk.main_quit)
+
+    #exhibits animation in gtk window
+    print('TODO: ShowAnimation')
+
+    #showing window and starting gtk main loop
+    window.show()
+    gtk.main()
+
+    return None
