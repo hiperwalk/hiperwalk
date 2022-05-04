e71622847131e5a6d1ea12eba89a7861b7dda5ef
Koruja <gbezerra@posgrad.lncc.br>
2022-05-04 09:43:17 -0300

    Creates animation module

---
+++ AnimationModule.py
@@ -0,0 +1,18 @@
+from Constants import DEBUG
+
+if DEBUG:
+    from time import time
+
+def CreateAnimation(imgs):
+    print('TODO: CreateAnimation')
+    anim = None
+    return anim
+
+def SaveAnimation(anim):
+    print('TODO: SaveAnimation')
+    return None
+
+def ShowAnimation(anim):
+    print('TODO: ShowAnimation')
+    return None
+
