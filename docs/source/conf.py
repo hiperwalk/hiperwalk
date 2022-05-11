1b67d572123b28233095d0fba5c32441fb595b6c
Koruja <gbezerra@posgrad.lncc.br>
2022-05-11 14:28:18 -0300

    Add sample function documentation autogeneration from code

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,9 +10,10 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+sys.path.append(os.path.abspath('../../'))
+print(sys.path)
 
 
 # -- Project information -----------------------------------------------------
@@ -32,6 +33,7 @@ release = '2.0'
 # ones.
 extensions = [
     'sphinx.ext.duration',
+    'sphinx.ext.autodoc',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
