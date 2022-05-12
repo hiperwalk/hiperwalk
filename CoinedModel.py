75c12e3dd14bc503add00f4db342e2d8fd4ae7b9
Gustavowl <gbezerra@posgrad.lncc.br>
2022-05-12 19:55:39 -0300

    Add FlipFlopShiftOperator documentation

--- CoinedModel.py
+++ CoinedModel.py
@@ -22,15 +22,124 @@ def UniformInitialCondition(AdjMatrix):
     #TODO: USE np.ones
 
 def FlipFlopShiftOperator(AdjMatrix):
-    """
-    Creates flip-flop shift operator based on adjacency matrix.
+    r"""
+    Creates flip-flop shift operator (:math:`S`) based on adjacency matrix.
     For more information about the general flip-flop shift operator,
     check "Quantum Walks and Search Algorithms" Section 7.2: Coined Walks on Arbitrary Graphs.
-    TODO: explain resulting matrix (edges labeled similarly to position-coin notation)
-    Parameter: expects adjacency matrix of an unweighted undirected graph
-    Expects sparse matrix #TODO: throw exception or convert
 
-    :param AdjMatrix: Adjacency Matrix.
+    For generating S, the position-coin notation is used alongside the
+    Hilbert space :math:`\mathcal{H}^{2|E|}`.
+    It is supposed that the elements of any state
+    :math:`\ket{\psi} \in \mathcal{H}^{2|E|}`
+    respect the sorted edges order,
+    i.e. :math:`(v, u) < (v', u')` if either :math:`v < v'` or
+    :math:`v = v'` and :math:`u < u'`
+    where :math:`(v, u), (v', u')` are valid edges.
+    The created Matrix also respects the sorted edges order.
+
+    For example, the graph :math:`G(V, E)` shown in figure **TODO** has adjacency matrix
+
+    .. math::
+        A = \begin{bmatrix}
+                0 & 1 & 0 & 0 \\
+                1 & 0 & 1 & 1 \\
+                0 & 1 & 0 & 1 \\
+                0 & 1 & 1 & 0
+            \end{bmatrix} .
+
+    The edges are labelled according to sorted order;
+    i.e. labelled in a left-to-right and top-to-bottom fashion.
+    For example, :math:`A_\text{labels}` has the appropriated labels for :math:`A`
+    where invalid edges are omitted.
+
+    .. math::
+        A_\text{labels} = \begin{bmatrix}
+                  & 0 &   &   \\
+                1 &   & 2 & 3 \\
+                  & 4 &   & 5 \\
+                  & 6 & 7 &  
+            \end{bmatrix} .
+
+    The edges labels are illustrated in figure **TODO**.
+
+    For consistency, any state :math:`\ket\psi \in \mathcal{H}^{2|E|}`
+    must respect the sorted edges order.
+    In other words,
+    :math:`\ket\psi = \begin{bmatrix}
+    \psi_0 & \psi_1 & \cdots & \psi_{2|E| - 1}
+    \end{bmatrix}^T`
+    where :math:`\psi_i`  corresponds to the **AMPLITUDE?** of the walker being
+    in vertex :math:`v` and the coin pointing towards vertex :math:`u`
+    given that :math:`(v, u)` is the :math:`i`-th edge
+    for :math:`i \in [0, 1, \ldots, 2|E| - 1]`.
+
+    .. note::
+        Labelling and sorting edges is done for computational purposes only.
+        We could simply use the `arc notation`:
+        
+        .. math::
+            \ket\psi = \sum_{i = 0}^{2|E| - 1} \psi_i \ket{i} =
+            \sum_{(v, u) \in E} \psi_{(v, u)} \ket{(v, u)}
+
+        for appropriate choices of :math:`\psi_{(v, u)}`.
+
+    In our example,
+
+    .. math::
+        \ket\psi = \begin{bmatrix} \psi_0 \\ \psi_1 \\ \vdots \\ \psi_7 \end{bmatrix},
+
+    where :math:`\psi_0` is the amplitude of the walker being in vertex 0
+    and the coin pointing towards vertex 1;
+    :math:`\psi_1` is the amplitude of the walker being in vertex 1
+    and the coin pointing towards vertex 0;
+    and so on.
+
+    The flip-flop shift operator :math:`S` is defined such that
+
+    .. math::
+        \begin{align*}
+            S \ket{(v, u)} &= \ket{(u, v)} \\
+            \implies S\ket i &= \ket j
+        \end{align*}
+
+    as long as :math:`i` is the label of edge :math:`(v, u)` and
+    :math:`j` is the label of edge :math:`(u, v)`.
+    As a consequence, in our example, the resulting flip-flop shift matrix is
+
+    .. math::
+        S =
+        \begin{bmatrix}
+            0 & 1 & 0 & 0 & 0 & 0 & 0 & 0 \\
+            1 & 0 & 0 & 0 & 0 & 0 & 0 & 0 \\
+            0 & 0 & 0 & 0 & 1 & 0 & 0 & 0 \\
+            0 & 0 & 0 & 0 & 0 & 0 & 1 & 0 \\
+            0 & 0 & 1 & 0 & 0 & 0 & 0 & 0 \\
+            0 & 0 & 0 & 0 & 0 & 0 & 0 & 1 \\
+            0 & 0 & 0 & 1 & 0 & 0 & 0 & 0 \\
+            0 & 0 & 0 & 0 & 0 & 1 & 0 & 0 
+        \end{bmatrix}.
+
+    Note that as required, :math:`S^2 = I`,
+    :math:`S \ket 0 = \ket 1`, :math:`\ket 1 = \ket 0`,
+    :math:`S \ket 2 = \ket 4`, :math:`S \ket 4 = \ket 2`, etc.
+
+
+    Parameters
+    ----------
+    AdjMatrix: scipy.sparse.csr_matrix
+        Adjacency Matrix of an unweighted undirected graph.
+    .. todo::
+        if not sparse, throw exception of convert to sparse.
+
+    Returns
+    -------
+    scipy.sparse.csr_matrix
+        Flip-flop shift operator.
+
+    Examples
+    --------
+    .. todo::
+        do some examples
     """
 
     if DEBUG:
