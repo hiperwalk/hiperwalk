d123b4e460287dbdf51389ee3e983eace1177ed3
Koruja <gbezerra@posgrad.lncc.br>
2022-05-13 15:38:12 -0300

    Configure autoclass template

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -51,6 +51,10 @@ todo_include_todos = True
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
+# Removes numpy auto documentation.
+# Documentation is generated recursively according to _templates files
+numpydoc_class_members_toctree = False
+
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
