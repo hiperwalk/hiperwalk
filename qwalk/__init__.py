e8899e39e5ab73bf9b59c24b6275db8b290b56ae
Koruja <gbezerra@posgrad.lncc.br>
2022-10-21 16:24:50 -0300

    Removes underscore from coined and basewalk

--- qwalk/__init__.py
+++ qwalk/__init__.py
@@ -1,5 +1,5 @@
 # importing everything under the hood of the
 # hiperwalk.qwalk package
-from ._base_walk import *
-from ._coined import *
+from .base_walk import *
+from .coined import *
 from .segment import *
