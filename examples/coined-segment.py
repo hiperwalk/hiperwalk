788977585f956b283ac1eeb5896ca629846a9abf
Koruja <gbezerra@posgrad.lncc.br>
2022-10-26 17:48:29 -0300

    Test if shift operator is unitary

--- examples/coined-segment.py
+++ examples/coined-segment.py
@@ -1,20 +1,21 @@
 import sys
 import numpy as np
+import scipy.sparse
 sys.path.append('..')
 import qwalk as qw
 import plot as hplot
 
-num_vert = 8
+num_vert = 5
 seg = qw.Segment(num_vert)
-print(seg.adj_matrix.todense())
-print(seg.flip_flop_shift_operator())
+# print(seg.adj_matrix.todense())
+# print(seg.flip_flop_shift_operator())
 
 init_cond = np.zeros(2*(num_vert-1), dtype=complex)
 # opposite order due to how Coined is implemented
 # encapsule this to the user
+# these initial conditions only work if num_vert is odd
 middle_right = num_vert - 1
 middle_left = num_vert - 2
-
 init_cond[middle_left] = 1
 # init_cond[middle_right] = 1/np.sqrt(2)
 # init_cond[middle_left] = -1j/np.sqrt(2)
@@ -23,6 +24,10 @@ init_cond[middle_left] = 1
 # U = seg.evolution_operator(coin='hadamard')
 # U = seg.flip_flop_shift_operator()
 U = seg.shift_operator()
+I = U.transpose() @ U
+print("Is unitary? " + str(
+    (I != scipy.sparse.identity(I.shape[0])).nnz == 0
+))
 num_steps = int(2*num_vert - 2)
 
 states = seg.simulate_walk(U, init_cond, num_steps, save_interval=1)
