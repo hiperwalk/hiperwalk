02c4a294921c03725ca9923703fa3dbaf48d41eb
Koruja <gbezerra@posgrad.lncc.br>
2022-10-27 18:30:51 -0300

    Implement coined state with arc_order

--- examples/coined-segment.py
+++ examples/coined-segment.py
@@ -10,15 +10,18 @@ seg = qw.Segment(num_vert)
 # print(seg.adj_matrix.todense())
 # print(seg.flip_flop_shift_operator())
 
-init_cond = np.zeros(2*(num_vert-1), dtype=complex)
 # opposite order due to how Coined is implemented
 # encapsule this to the user
 # these initial conditions only work if num_vert is odd
 middle_right = num_vert - 1
 middle_left = num_vert - 2
-init_cond[middle_left] = 1
+# init_cond[middle_left] = 1
 # init_cond[middle_right] = 1/np.sqrt(2)
 # init_cond[middle_left] = -1j/np.sqrt(2)
+init_cond = seg.state([middle_right, middle_left],
+                      amplitudes=np.array([1, -1j]),
+                      type="arc_order")
+print(init_cond)
 
 
 # U = seg.evolution_operator(coin='hadamard')
