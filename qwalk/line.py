2caf413e769f4629627efd4de5d3d2ba548cd808
Koruja <gbezerra@posgrad.lncc.br>
2022-12-16 12:22:42 -0300

    Add coined line docs

--- qwalk/line.py
+++ qwalk/line.py
@@ -3,16 +3,31 @@ from .segment import Segment
 
 class Line(Segment):
     r"""
-    Add documentation
+    Class for managing quantum walk on the line.
+
+    For simulating the quantum walk on the "infinite" line,
+    it is necessary to know the initial condition and the
+    number of steps beforehand
+    in order to allocate the necessary resources.
+
+    Parameters
+    ----------
+    num_steps : int
+        Number of steps to be simulated.
+    state_entries : tuple
+        Check :meth:`state` for details of valid entries.
+    entry_type : {'vertex_dir', 'arc_notation', 'arc_order'}
+        Check :meth:`state` for details.
+
+    Notes
+    -----
+    It is built on top of :class:`Segment`,
+    so a walk on the line is not simulated.
+    But rather a walk on a sufficiently large segment.
+
     """
 
     def __init__(self, num_steps, state_entries, entry_type='vertex_dir'):
-        r"""
-        .. todo::
-            Check which is the rightmost vertex pointing righttowards
-            and the leftmost vertex pointing leftwards
-        """
-        
         valid_entry_types = ['vertex_dir', 'arc_notation']
         if entry_type not in valid_entry_types:
             raise ValueError("Invalid argument for entry_type."
@@ -27,10 +42,7 @@ class Line(Segment):
                 state_entries, entry_type
         )
 
-        print(state_entries)
-        print(shifted_entries)
         num_vert = right_vert - self._shift + num_steps + 1
-        print(num_vert)
 
         super().__init__(num_vert)
 
@@ -68,5 +80,12 @@ class Line(Segment):
 
     def simulate_walk(self, evolution_operator, save_interval=0,
                       hpc=False):
+        r"""
+        Starts quantum walk simulation.
+
+        Since the initial condition and number of steps must
+        passed as arguments to the constructor,
+        the respective parameters are ommited.
+        """
         super().simulate_walk(evolution_operator, self._initial_condition,
                               self._num_steps, save_interval, hpc)
