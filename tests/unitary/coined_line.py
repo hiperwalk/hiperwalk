fdd267f659891d765dd6f91f2cce34e5ca05bb74
Koruja <gbezerra@posgrad.lncc.br>
2023-03-09 11:33:32 -0300

    Add coined line initial condition test

--- tests/unitary/coined_line.py
+++ tests/unitary/coined_line.py
@@ -7,6 +7,36 @@ import unittest
 
 class TestCoinedLine(unittest.TestCase):
 
+    def test_initial_condition_in_middle(self):
+        # middle right
+        num_steps = 5
+        entries = [[1, 0, 0]]
+        line = hpline.Line(num_steps, entries)
+
+        S = line.flip_flop_shift_operator()
+        I = S@S
+        init_state = line.simulate_walk(I)[0]
+
+        self.assertTrue(
+            np.all(init_state[:2*num_steps] == 0)
+            and init_state[2*num_steps] == 1
+            and np.all(init_state[2*num_steps + 1:] == 0)
+        )
+
+
+        # middle left
+        num_steps = 5
+        entries = [[1, 0, 1]]
+        line = hpline.Line(num_steps, entries)
+
+        init_state = line.simulate_walk(I)[0]
+
+        self.assertTrue(
+            np.all(init_state[:2*num_steps - 1] == 0)
+            and init_state[2*num_steps - 1] == 1
+            and np.all(init_state[2*num_steps:] == 0)
+        )
+
     def test_persistent_shift_right_state_transfer(self):
         # initial state in leftmost vertex
         # final state in rightmost vertex
