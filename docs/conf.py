12ad56d80143eee5d760444e7a085d671f383a27
Gustavowl <gbezerra@posgrad.lncc.br>
2023-03-23 12:13:03 -0300

    Add alpha release level to docs

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2023'
 author = 'Gustavo Bezerra'
 
 # The full version, including alpha/beta/rc tags
-release = '2.0.0'
+release = '2.0.0a'
 
 
 # -- General configuration ---------------------------------------------------
