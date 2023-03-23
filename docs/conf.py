9ddc6ea506cae8505f7670fca1c6dc0d37ce8dfe
hiperwalk <hiperwalk@gmail.com>
2023-03-23 08:17:21 -0300

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -78,3 +78,5 @@ html_theme = 'pydata_sphinx_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = []
+
+master_doc = 'index'
