b194a8246aa170f010389143847bdf53960807dd
Koruja <gbezerra@posgrad.lncc.br>
2023-04-10 11:02:01 -0300

    Update coined line example

--- examples/coined-line.py
+++ examples/coined-line.py
@@ -2,20 +2,17 @@ import sys
 import numpy as np
 import scipy.sparse
 sys.path.append('..')
-import qwalk as qw
+import qwalk.coined as coined_qw
 import plot as hplot
 
-# state_entries = [[1, 0, 0], [1, -1, 1]] # vertex_dir
-# line = qw.Line(10, state_entries)
-
 state_entries = [[1, 0, 1], [1, -1, -2]] # arc_notation
-line = qw.Line(10, state_entries, 'arc_notation')
-print(state_entries)
+num_steps = 10
+line = coined_qw.Line(num_steps, state_entries, 'arc_notation')
 
-# U = seg.evolution_operator(coin='hadamard')
-# num_steps = int(num_vert/2)
-# 
-# states = seg.simulate_walk(U, init_cond, num_steps, save_interval=1)
-# prob = seg.probability_distribution(states)
-# hplot.plot_probability_distribution(prob, plot_type='line',
-#                                     filename_prefix="segment/anim", show=False)
+U = line.evolution_operator(coin='hadamard') 
+states = line.simulate_walk(U, save_interval=1)
+
+prob = line.probability_distribution(states)
+hplot.plot_probability_distribution(prob, plot_type='line',
+                                    filename_prefix="coined-line",
+                                    animate=True, show=True)
