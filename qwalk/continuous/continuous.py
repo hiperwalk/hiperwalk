059566352d24bbe1e42de63d651b54608b6f63fd
Koruja <gbezerra@posgrad.lncc.br>
2023-04-13 07:43:54 -0300

    Create continuous quantum walk class

--- qwalk/continuous/continuous.py
+++ qwalk/continuous/continuous.py
@@ -1,1 +1,23 @@
-# TODO
+from ..base_walk import BaseWalk
+from constants import DEBUG
+
+class Continuous(BaseWalk):
+    """
+    TODO: Documentation
+    """
+
+    def __init__(self, adj_matrix):
+        return None
+
+    def oracle(self, vertices):
+        return None
+
+    def evolution_operator(self, hpc=False, **kwargs):
+        return None
+
+    def search_evolution_operator(self, vertices, hpc=False, **kwargs):
+        return None
+
+    def probability_distribution(self, states):
+        return None
+
