ec41c00ed124bbb9de80290cda600f87b049eb38
Koruja <gbezerra@posgrad.lncc.br>
2023-04-13 08:31:58 -0300

    Autogenerate continuous quantum walk docs

--- qwalk/continuous/continuous.py
+++ qwalk/continuous/continuous.py
@@ -3,7 +3,28 @@ from constants import DEBUG
 
 class Continuous(BaseWalk):
     """
-    TODO: Documentation
+    Manage instance of the continuous time quantum walk model
+    on unweighted graphs.
+
+    Parameters
+    ----------
+    adj_matrix : :class:`scipy.sparse.csr_array`
+        Adjacency matrix of the graph on which
+        the quantum walk occurs.
+
+        .. todo::
+            * Accept other types such as numpy array
+
+    Notes
+    -----
+    This class can be used to simulate the evolution of any
+    continuous time matrix in the format
+
+    .. math::
+
+        U = e^{\mathrm{i}H}
+
+    as long as :math:`H` is hermitian.
     """
 
     def __init__(self, adj_matrix):
