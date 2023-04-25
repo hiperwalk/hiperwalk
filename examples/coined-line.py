8821d1829bf17ae08bbfd5571364a75a192a8b7e
Koruja <gbezerra@posgrad.lncc.br>
2023-04-25 11:23:06 -0300

    Update coined examples

--- examples/coined-line.py
+++ examples/coined-line.py
@@ -1,16 +1,16 @@
 import sys
 sys.path.append('..')
 import qwalk.coined as coined_qw
-import plot as hplot
+import qwplot
 
 state_entries = [[1, 0, 1], [1, -1, -2]] # arc_notation
 num_steps = 10
-line = coined_qw.Line((0, num_steps), state_entries, 'arc_notation')
+line = coined_qw.Line((num_steps, 1), state_entries, 'arc_notation')
 
 U = line.evolution_operator() 
 states = line.simulate()
 
 prob = line.probability_distribution(states)
-hplot.plot_probability_distribution(prob, plot_type='line',
-                                    filename_prefix="coined-line",
-                                    animate=True, show=True)
+qwplot.probability_distribution(prob, plot_type='line',
+                                filename_prefix="coined-line",
+                                animate=True, show=True)
--- examples/coined-segment.py
+++ examples/coined-segment.py
@@ -1,7 +1,7 @@
 import sys
 sys.path.append('..')
 import qwalk.coined as coined_qw
-import plot as hplot
+import qwplot
 
 num_vert = 21
 mid_vert = int(num_vert/2)
@@ -10,15 +10,14 @@ seg = coined_qw.Segment(num_vert)
 # Initial condition starting in the middle vertex
 # with the coin pointing to the right (real amplitude)
 # and the left (complex amplitude).
-seg.initial_condition(
+psi0 = seg.state(
     [(1, mid_vert, 0), [-1j, mid_vert, 1]]
 )
 
 hpcU = seg.evolution_operator()
 
 num_steps = 15
-seg.step((0, num_steps))
-states = seg.simulate()
+states = seg.simulate((num_steps, 1), psi0, hpcU)
 
 prob = seg.probability_distribution(states)
-hplot.plot_probability_distribution(prob, animate=True, plot_type='line')
+qwplot.probability_distribution(prob, animate=True, plot_type='line')
