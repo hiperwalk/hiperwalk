ce95f4c55c76f58b00e7754e4ff1b2f3306c614a
Koruja <gbezerra@posgrad.lncc.br>
2023-04-25 11:00:56 -0300

    Update plot locator

--- examples/coined-cycle.py
+++ examples/coined-cycle.py
@@ -1,18 +1,16 @@
 import sys
 sys.path.append('..')
 import qwalk.coined as coined_qw
-import plot as hplot
+import qwplot
 
 num_vert = 20
 cycle = coined_qw.Cycle(num_vert)
 
-cycle.initial_condition([(1, 0, 0)])
+psi0 = cycle.state([(1, 0, 0)])
 
 S = cycle.persistent_shift_operator()
-cycle.set_evolution_operator(S)
-cycle.step((0, num_vert))
 
-states = cycle.simulate()
+states = cycle.simulate((num_vert, 1), psi0, S)
 
 prob = cycle.probability_distribution(states)
-hplot.plot_probability_distribution(prob, plot_type='bar', animate=True)
+qwplot.probability_distribution(prob, plot_type='bar', animate=True)
