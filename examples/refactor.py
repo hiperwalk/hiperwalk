4f14c59f82e45c0835ffe2e5a7c0f82359c695fa
Koruja <gbezerra@posgrad.lncc.br>
2023-05-10 15:07:01 -0300

    Add all Graph queries to _set_flipflop_shift

--- examples/refactor.py
+++ examples/refactor.py
@@ -3,12 +3,12 @@ sys.path.append('..')
 import hiperwalk as hpw
 import networkx as nx
 
-num_vert = 40000
+num_vert = 10
 adj_matrix = nx.adjacency_matrix(nx.cycle_graph(num_vert))
 g = hpw.Graph(adj_matrix)
+print(g.number_of_vertices())
+print(g.number_of_arcs())
+print(g.number_of_edges())
 
-dtqw = hpw.CoinedWalk(g)
-print(dtqw)
-print(dtqw._shift.todense())
-print(dtqw._coin.todense())
-print(dtqw._marked)
+qw = hpw.CoinedWalk(g)
+print(qw._shift.todense())
