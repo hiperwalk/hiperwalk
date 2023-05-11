f56e5bbcd14cdfdabe09960c1713ba5b4944bec3
Koruja <gbezerra@posgrad.lncc.br>
2023-05-11 16:54:32 -0300

    Update coined get_evolution

--- examples/refactor.py
+++ examples/refactor.py
@@ -2,31 +2,15 @@ import sys
 sys.path.append('..')
 import hiperwalk as hpw
 import networkx as nx
+import numpy as np
 
-num_vert = 10
+num_vert = 5
 adj_matrix = nx.adjacency_matrix(nx.cycle_graph(num_vert))
 g = hpw.Graph(adj_matrix)
-print(adj_matrix.indices)
-print(adj_matrix.indptr)
-print(list(map(g.degree, range(num_vert))))
 
 coin = ['H'] * num_vert
-coin[-1] = coin[0] = '-I'
 qw = hpw.CoinedWalk(g, coin=coin)
-print(qw._marked)
-print(qw._marked_coin)
-
-qw.set_marked([1, 2, 3])
-print()
-print(qw._marked)
-print(qw._marked_coin)
-
-qw.set_marked({'-H' : 1, '-G' : [3, 2], '-I' : 7})
-print()
-print(qw._marked)
-print(qw._marked_coin)
-
-qw.set_marked()
-print()
-print(qw._marked)
-print(qw._marked_coin)
+print(not np.any(
+    qw.get_evolution().todense()
+    - qw.get_shift() @ qw.get_final_coin()
+))
