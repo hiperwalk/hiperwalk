5224b2a756bc7f9de82821e6725b59f0d5e189c3
Koruja <gbezerra@posgrad.lncc.br>
2023-05-12 09:32:12 -0300

    Refactor coined state

--- examples/refactor.py
+++ examples/refactor.py
@@ -6,14 +6,14 @@ import numpy as np
 import scipy.sparse
 
 num_vert = 5
-#adj_matrix = nx.adjacency_matrix(nx.cycle_graph(num_vert))
-adj_matrix = scipy.sparse.csr_array(
-            [[0, 1, 1, 1, 1],
-             [1, 0, 1, 1, 0],
-             [1, 1, 0, 0, 0],
-             [1, 1, 0, 0, 0],
-             [1, 0, 0, 0, 0]]
-        )
+adj_matrix = nx.adjacency_matrix(nx.cycle_graph(num_vert))
+#adj_matrix = scipy.sparse.csr_array(
+#            [[0, 1, 1, 1, 1],
+#             [1, 0, 1, 1, 0],
+#             [1, 1, 0, 0, 0],
+#             [1, 1, 0, 0, 0],
+#             [1, 0, 0, 0, 0]]
+#        )
 g = hpw.Graph(adj_matrix)
 
 coin = ['G'] * num_vert
@@ -24,9 +24,6 @@ print(not np.any(
 ))
 
 num_arcs = g.number_of_arcs()
-state = np.ones(num_arcs) / np.sqrt(num_arcs)
-print([g.degree(v) for v in range(num_vert)])
-print(qw.probability_distribution(state))
-state = np.zeros(num_arcs)
-state[0] = state[-1] = 1/np.sqrt(2)
-print(qw.probability_distribution(state))
+state = qw.uniform_state()
+print(state)
+print(qw.state([1, 0], [1, 1], [2, (1, 0)]))
