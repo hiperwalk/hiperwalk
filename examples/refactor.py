7fab130dea4021006dd651b67ab69c4757d000a0
Koruja <gbezerra@posgrad.lncc.br>
2023-05-24 08:16:41 -0300

    Fix periodic horizontal lattice adjacency

--- examples/refactor.py
+++ examples/refactor.py
@@ -4,14 +4,16 @@ import hiperwalk as hpw
 import networkx as nx
 import numpy as np
 
-dim = 10
-l = hpw.Lattice(dim, periodic=True, diagonal=True)
+dim = 4
+l = hpw.Lattice(dim, periodic=False, diagonal=False)
+print(l.adj_matrix.indptr)
+print(l.adj_matrix.indices)
 
-qw = hpw.CoinedWalk(graph=l, coin='I')
-
-psi0 = qw.ket(0, dim + 1)
-states = qw.simulate((0, dim//2, 1), psi0, False)
-print(states)
-probs = qw.probability_distribution(states)
-
-hpw.plot_probability_distribution(probs, graph=l)
+#qw = hpw.CoinedWalk(graph=l, coin='I')
+#
+#psi0 = qw.ket(0, dim + 1)
+#states = qw.simulate((0, dim//2, 1), psi0, False)
+#print(states)
+#probs = qw.probability_distribution(states)
+#
+#hpw.plot_probability_distribution(probs, graph=l)
