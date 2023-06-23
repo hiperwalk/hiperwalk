0ee826d3258a19b03ba0f7929995c3943094a985
hiperwalk <hiperwalk@gmail.com>
2023-06-23 10:26:03 -0300

    Update line.py

--- hiperwalk/graph/line.py
+++ hiperwalk/graph/line.py
@@ -10,44 +10,33 @@ class Line(Graph):
     Parameters
     ----------
     num_vert : int
-        Number of vertices on the line.
+        The number of vertices on the line.
 
     Notes
     -----
-    On the Line,
-    we can assign directions to the arcs.
-    An arc pointing rightward has direction 0 --
-    i.e., (1, 2), and
-    an arc pointing leftward has direction 1 --
-    i.e., (2, 1).
+    In the :obj:`Line` class, directions can be assigned to the arcs. 
+    An arc pointing to the right has direction 0 (e.g., (1, 2)), 
+    and an arc pointing to the left has direction 1 (e.g., (2, 1)).
 
-    We use the directions of the arcs to define their order.
-    Thus, for a vertex :math:`v \in V`,
-    the arcs :math:`(v, v + 1)` and :math:`(v, v - 1)`
-    have labels :math:`a_0` and :math:`a_1` (respectively)
-    such that  :math:`a_0 < a_1`.
-    This order is aligned with the description of
-    coined quantum walks on the line in Reference [1]_.
-    The only exceptions to this rule are the extreme vertices
-    0 and :math:`|V| - 1` because
-    they only have one arc.
-    Aside from these exceptions,
-    for any two vertices :math:`v_1 < v_2`,
-    any arc with tail :math:`v_1` has a label smaller than
-    the label of any arc with tail :math:`v_2`.
+    The order of the arcs is determined by their direction. 
+    Thus, for a vertex :math:`v \in V`, 
+    the arcs :math:`(v, v + 1)` and :math:`(v, v - 1)` 
+    have labels :math:`a_0` and :math:`a_1` respectively, 
+    with :math:`a_0 < a_1`. 
+    The only exceptions to this rule are the extreme vertices 0 
+    and :math:`|V| - 1`, as they have outdegree 1. 
 
-    For example, Figure 1 depicts the labels of the arcs of a path graph
-    with 4 vertices.
+    Apart from these exceptions, 
+    for any two vertices :math:`v_1 < v_2`, 
+    any arc with tail :math:`v_1` will have a label smaller than the 
+    label of any arc with tail :math:`v_2`.
+
+    For instance, Figure 1 illustrates the labels of the arcs of a path graph with 4 vertices.
     
     .. graphviz:: ../../graphviz/line-arcs.dot
         :align: center
         :layout: neato
         :caption: Figure 1.
-
-    References
-    ----------
-    .. [1] R. Portugal. "Quantum walks and search algorithms",
-        2nd edition, Springer, 2018.
     """
 
     def __init__(self, num_vert):
