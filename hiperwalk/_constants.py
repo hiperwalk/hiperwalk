359c02ead3a695f457e0edbfef5ff896c8485082
Koruja <gbezerra@posgrad.lncc.br>
2023-07-04 18:41:41 -0300

    Fix integration with new pyneblina

--- hiperwalk/_constants.py
+++ hiperwalk/_constants.py
@@ -1,10 +1,5 @@
 __version__ = '2.0a4'
 
-#Declares data types according to neblina-core
-#TODO: make it so it is not hardcoded
-NEBLINA_FLOAT = 2
-NEBLINA_COMPLEX = 13 
-
 from sys import modules as sys_modules
 
 __USING_PDB__ = 'pdb' in sys_modules
