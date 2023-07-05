0eff57889f4ff8a89990375fef3614db6ac3f5f3
Koruja <gbezerra@posgrad.lncc.br>
2023-07-05 16:09:01 -0300

    Update coined cycle tests

--- tests/unitary/coined_cycle.py
+++ tests/unitary/coined_cycle.py
@@ -3,53 +3,55 @@ from sys import path as sys_path
 sys_path.append('../')
 sys_path.append('../../')
 from test_constants import *
-import qwalk.coined as hpcoined
+import hiperwalk as hpw
 import unittest
 
 class TestCoinedCycle(unittest.TestCase):
 
+    def setUp(self):
+        self.num_vert = 10
+        self.cycle = hpw.Cycle(self.num_vert)
+        self.qw = hpw.CoinedWalk(self.cycle)
+
+
     @unittest.skipIf(not TEST_NONHPC, 'Skipping nonhpc tests.')
     def test_clockwise_roundabout(self):
-        num_steps = 20
-        cycle = hpcoined.Cycle(num_steps)
+        self.qw.set_shift('persistent')
+        self.qw.set_coin('I')
+        self.qw.set_marked([])
 
-        S = cycle.persistent_shift_operator()
-        init_state = cycle.state([(1, 0, 0)], 'vertex_dir')
+        init_state = self.qw.state([1, (0, 1)])
 
-        final_state = cycle.simulate(num_steps, init_state,
-                                     S, hpc=False)[0]
+        num_steps = self.num_vert
+        final_state = self.qw.simulate(num_steps, init_state, hpc=False)[0]
 
         self.assertTrue(np.all(init_state == final_state))
 
     @unittest.skipIf(not TEST_NONHPC, 'Skipping nonhpc tests.')
     def test_anticlockwise_roundabout(self):
-        num_steps = 20
-        cycle = hpcoined.Cycle(num_steps)
+        self.qw.set_shift('persistent')
+        self.qw.set_coin('I')
+        self.qw.set_marked([])
 
-        S = cycle.persistent_shift_operator()
-        init_state = cycle.state([(1, 0, 1)], 'vertex_dir')
+        init_state = self.qw.state([1, (0, self.num_vert - 1)])
 
-        final_state = cycle.simulate(num_steps, init_state,
-                                     S, hpc=False)[0]
+        num_steps = self.num_vert
+        final_state = self.qw.simulate(num_steps, init_state, hpc=False)[0]
 
         self.assertTrue(np.all(init_state == final_state))
 
     @unittest.skipIf(not TEST_NONHPC, 'Skipping nonhpc tests.')
     def test_hadamard_evolution_operator(self):
-        num_vert = 10
-        cycle = hpcoined.Cycle(num_vert)
+        init_state = self.qw.state((1, (0, 1)))
 
-        cycle.evolution_operator(hpc=False)
-        init_state = cycle.state([(1, 0, 0)], 'vertex_dir')
-
-        num_steps = num_vert*2
-        states = cycle.simulate((num_steps, 1), init_state, hpc=False)
+        num_steps = 2*self.num_vert
+        states = self.qw.simulate((num_steps, 1), init_state, hpc=False)
         states = states.real
 
         def recursive_expression():
             states = np.zeros((num_steps + 1, init_state.shape[0]))
             states[0] = np.copy(init_state)
-            num_arcs = 2*num_vert
+            num_arcs = 2*self.num_vert
 
             for i in range(1, num_steps + 1):
                 for j in range(init_state.shape[0]):
@@ -65,51 +67,44 @@ class TestCoinedCycle(unittest.TestCase):
 
 
         rec_states = recursive_expression()
-
-        print(states)
-        print(rec_states)
         
         self.assertTrue(np.allclose(states, rec_states,
                                     rtol=1e-15, atol=1e-15))
 
     @unittest.skipIf(not TEST_HPC, 'Skipping hpc tests.')
     def test_hpc_clockwise_roundabout(self):
-        num_steps = 20
-        cycle = hpcoined.Cycle(num_steps)
+        self.qw.set_shift('persistent')
+        self.qw.set_coin('I')
+        self.qw.set_marked([])
 
-        S = cycle.persistent_shift_operator()
-        init_state = cycle.state([(1, 0, 0)], 'vertex_dir')
+        init_state = self.qw.state([1, (0, 1)])
 
-        final_state = cycle.simulate(num_steps, init_state,
-                                     S, hpc=True)[0]
+        num_steps = self.num_vert
+        final_state = self.qw.simulate(num_steps, init_state, hpc=True)[0]
 
         self.assertTrue(np.all(init_state == final_state))
 
     @unittest.skipIf(not TEST_HPC, 'Skipping hpc tests.')
     def test_hpc_anticlockwise_roundabout(self):
-        num_steps = 20
-        cycle = hpcoined.Cycle(num_steps)
+        self.qw.set_shift('persistent')
+        self.qw.set_coin('I')
+        self.qw.set_marked([])
 
-        S = cycle.persistent_shift_operator()
-        init_state = cycle.state([(1, 0, 1)], 'vertex_dir')
+        init_state = self.qw.state([1, (0, self.num_vert - 1)])
 
-        final_state = cycle.simulate(num_steps, init_state,
-                                     S, hpc=True)[0]
+        num_steps = self.num_vert
+        final_state = self.qw.simulate(num_steps, init_state, hpc=True)[0]
 
         self.assertTrue(np.all(init_state == final_state))
 
     @unittest.skipIf(not TEST_HPC, 'Skipping hpc tests.')
     def test_hpc_evolution_operator_matches_nonhpc(self):
-        num_vert = 10
-        cycle = hpcoined.Cycle(num_vert)
+        init_state = self.qw.state([1, (0, 1)])
+        num_steps = 2*self.num_vert
 
-        U = cycle.evolution_operator(hpc=True)
-        init_state = cycle.state([(1, 0, 0)], 'vertex_dir')
-        num_steps = num_vert*2
-
-        states = cycle.simulate(
+        states = self.qw.simulate(
             (num_steps, 1), init_state, hpc=False)
-        hpc_states = cycle.simulate(
+        hpc_states = self.qw.simulate(
             (num_steps, 1), init_state, hpc=True)
         
         self.assertTrue(np.allclose(states, hpc_states,
