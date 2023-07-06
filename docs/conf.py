527e0b6874639d0fdab8d19c32382e22a5e8916f
Koruja <gbezerra@posgrad.lncc.br>
2023-07-06 19:10:48 -0300

    Update switcher for stable version

--- docs/conf.py
+++ docs/conf.py
@@ -22,8 +22,8 @@ copyright = '2023'
 author = 'Gustavo Bezerra'
 
 # The full version, including alpha/beta/rc tags
-release = 'latest'
-version = 'latest'
+release = '2.0a5'
+version = 'stable'
 
 
 # -- General configuration ---------------------------------------------------
