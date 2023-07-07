737d21d3db3b3003ea8b630936de756e1b17338c
Koruja <gbezerra@posgrad.lncc.br>
2023-07-07 17:23:05 -0300

    Update version to 2.0a6

--- docs/conf.py
+++ docs/conf.py
@@ -22,8 +22,8 @@ copyright = '2023'
 author = 'Gustavo Bezerra'
 
 # The full version, including alpha/beta/rc tags
-release = 'latest'
-version = 'latest'
+release = 'stable'
+version = 'stable'
 
 
 # -- General configuration ---------------------------------------------------
