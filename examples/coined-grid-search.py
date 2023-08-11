5bd1a9bed752e396e0db045c1fabc74db241eb2d
Koruja <gbezerra@posgrad.lncc.br>
2023-08-11 15:29:07 -0300

    Add quantum search on a grid example

---
+++ examples/coined-grid-search.py
@@ -0,0 +1,18 @@
+import hiperwalk as hpw
+import matplotlib.pyplot as plt
+import numpy as np
+import scipy
+
+dim = 25
+g = hpw.Grid((dim, dim))
+N = g.number_of_vertices()
+qw = hpw.Coined(graph=g, coin='G', shift='ff', marked={'-I': 0})
+psi0 = qw.uniform_state()
+num_steps = int(1.5*np.sqrt(N*np.log(N)))
+states = qw.simulate(time=(num_steps, 1),
+                     initial_state=psi0,
+                     hpc=False)
+mark_prob = qw.marked_probability(states)
+plt.plot(list(range(num_steps + 1)), mark_prob,
+         marker='o')
+plt.show()
