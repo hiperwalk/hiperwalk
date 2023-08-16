3048c5299680a6676c65221dedc4d16f7c4ed3ad
Koruja <gbezerra@posgrad.lncc.br>
2023-08-16 09:58:48 -0300

    Remove all references to 'adj_matrix' keyword for plots

--- hiperwalk/plot/_plot.py
+++ hiperwalk/plot/_plot.py
@@ -352,8 +352,7 @@ def _preconfigure_graph_plot(probabilities, kwargs):
     Set parameters that need not to be changed for multiple plots
     or animation of a quantum walk.
     For example: the graph.
-    The graph must be sent via kwargs,
-    either via the 'graph' or the 'adj_matrix' keywords.
+    The graph must be sent via kwargs via the 'graph' keyword.
 
     Parameters
     ----------
@@ -381,11 +380,6 @@ def _preconfigure_graph_plot(probabilities, kwargs):
     elif isinstance(graph, Graph):
         adj_matrix = graph.adjacency_matrix()
         kwargs['graph'] = nx.from_scipy_sparse_array(adj_matrix)
-
-    if 'adj_matrix' in kwargs:
-        # Pops adj_matrix if both graph and adj_matrix keywords are set;
-        # otherwise invalid keyword may be raisen by networkx
-        kwargs.pop('adj_matrix')
 
     if 'min_node_size' not in kwargs:
         kwargs['min_node_size'] = None
