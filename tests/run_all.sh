10ea6a82398cd098ddf96b4c8863e678c8278584
Koruja <gbezerra@posgrad.lncc.br>
2023-08-17 20:29:56 -0300

    Rename unitary directory to unit

--- tests/run_all.sh
+++ tests/run_all.sh
@@ -1,3 +1,3 @@
 echo "TEST_HPC = True" > test_constants.py
 echo "TEST_NONHPC = True" >> test_constants.py
-python3 -m unittest unitary/*.py
+python3 -m unittest unit/*.py
--- tests/run_nonhpc.sh
+++ tests/run_nonhpc.sh
@@ -1,3 +1,3 @@
 echo "TEST_HPC = False" > test_constants.py
 echo "TEST_NONHPC = True" >> test_constants.py
-python3 -m unittest unitary/*.py
+python3 -m unittest unit/*.py
