f8e25ffd030f171c5a31c7e7a18d7a1e16db953e
Koruja <gbezerra@posgrad.lncc.br>
2023-10-11 17:23:47 -0300

    Implement quantum walk generator

--- hiperwalk/quantum_walk/generator.py
+++ hiperwalk/quantum_walk/generator.py
@@ -1,3 +1,26 @@
 
 def quantum_walk_generator(model, **kwargs):
-    return None
+
+    # Filter each key type.
+    function = []
+    iterable = []
+
+    for key in kwargs:
+        if callable(kwargs[key]):
+            function.append(key)
+        else:
+            iterable.append(key)
+            kwargs[key] = iter(kwargs[key])
+
+    while (True):
+        try:
+            qw_kwargs = {key : next(kwargs[key])
+                         for key in iterable}
+        except StopIteration:
+            return
+
+        func_args = qw_kwargs.pop('func_args')
+        for key in function:
+            qw_kwargs[key] = kwargs[key](func_args)
+
+        yield model(**qw_kwargs)
