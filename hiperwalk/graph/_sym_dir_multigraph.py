9cb89aef0eb451321667668383ca0be84f59f671
Koruja <gbezerra@posgrad.lncc.br>
2024-02-23 19:50:38 -0300

    Update symmetric directed multigraph constructor

--- hiperwalk/graph/_sym_dir_multigraph.py
+++ hiperwalk/graph/_sym_dir_multigraph.py
@@ -9,32 +9,9 @@ class SDMultigraph(Graph):
     """
 
     def __init__(self, graph):
-        # underlying muligraph
+        # underlying multigraph
         self.graph = graph
-
-        # store the label of the first arc with tail in each vertex
-        adj_matrix = graph.adjacency_matrix()
-        data = adj_matrix.data
-        first_arc = data.copy()
-
-        degree = 0
-        arc = 0
-        for i in range(len(first_arc)):
-            degree = first_arc[i]
-            first_arc[i] = arc
-            arc += degree
-                
-
-        print('-----------------------------')
-        print(data)
-        print(first_arc)
-        print('-----------------------------')
-
-        adj_matrix.data = first_arc
-        print(adj_matrix.todense())
-            
-
-        # change the pointers again
+        # TODO: different behavior if graph is simple or multigraph
 
     def arc(self, number):
         r"""
