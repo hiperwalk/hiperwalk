edde9e83bced212a956c67fee51af04a69de0f16
Koruja <gbezerra@posgrad.lncc.br>
2024-02-23 19:12:52 -0300

    Change multigraph internal storage

--- hiperwalk/graph/multigraph.py
+++ hiperwalk/graph/multigraph.py
@@ -7,10 +7,20 @@ class Multigraph(Graph):
 
     Parameters
     ----------
-    adj_matrix : :class:`scipy.sparse.csr_array`, :class:`numpy.ndarray` or :class:`networkx.Graph`
-        Adjacency matrix, Laplacian matrix, or any real Hermitian matrix.
+    adj_matrix :
+        The adjacency matrix of the graph
+        (any integer Hermitian matrix).
+        Two input types are accepted:
 
-        If :class:`network.Graph`, the adjacency matrix of the graph is used.
+        * Any matrix -- for instance,
+            * :class:`scipy.sparse.csr_array`,
+            * :class:`numpy.ndarray`,
+            * list of lists.
+        * :class:`network.Graph`.
+            * The adjacency matrix is extracted from the graph.
+
+    copy : bool, default=False
+        If ``True``, a hard copy of ``adj_matrix`` is stored.
 
     Raises
     ------
@@ -19,12 +29,14 @@ class Multigraph(Graph):
 
     Notes
     -----
-    .. todo::
-        Check if it is more efficient to store the adjacency matrix as
-        sparse or dense.
+    The ``adj_matrix.data`` attribute is changed for
+    more efficient manipulation.
+    If the original matrix is needed,
+    invoke the constructor with ``copy=True`` or
+    call :meth:`adjacency_matrix()` after creating the multigraph.
     """
 
-    def __init__(self, adj_matrix):
+    def __init__(self, adj_matrix, copy=False):
         try:
             adj_matrix.adj #throws AttributeError if not networkx graph
             import networkx as nx
@@ -33,45 +45,34 @@ class Multigraph(Graph):
             pass
 
         if not issparse(adj_matrix):
-            adj_matrix = csr_array(adj_matrix, dtype=np.int8)
+            adj_matrix = csr_array(adj_matrix)
 
         if adj_matrix.shape[0] != adj_matrix.shape[1]:
             raise TypeError("Adjacency matrix is not square.")
 
-        # the following is commented because the current way to
-        # implement Laplacian in ContinuousTime quantum walks is by
-        # passing the Laplacian as adjacency matrix
-        # if adj_matrix.data.min() != 1 or adj_matrix.data.max() != 1:
-        #     raise ValueError("Adjacency matrix must only have 0's "
-        #                      + "and 1's as entries.")
+        if copy:
+            adj_matrix = adj_matrix.copy()
+
+        # manipulate data
+        data = adj_matrix.data
+        for i in range(1, len(data)):
+            data[i] += data[i - 1]
 
         self._adj_matrix = adj_matrix
-        self._coloring = None
 
         # TODO: is it useful to store the underlying simple graph?
 
-    def neighbors(self, vertex):
-        r"""
-        Return all neighbors of the given vertex.
-        """
-        raise NotImplementedError()
-
     def number_of_edges(self):
-        r"""
-        Determine the cardinality of the edge set.
-        """
-        return self._adj_matrix.sum() >> 1
+        return self._adj_matrix.data[-1] >> 1
 
     def degree(self, vertex):
-        r"""
-        Return the degree of the given vertex.
+        vertex = self.vertex_number(vertex)
 
-        The degree of a vertex :math:`u` in a graph 
-        is the number of edges 
-        incident to :math:`u`. Loops at :math:`u` are counted once, 
-        reflecting the treatment of a loop at vertex :math:`u` as 
-        a single arc :math:`(u, u)`.
-        """
-        raise NotImplementedError()
+        if vertex == 0:
+            return adj_matrix.data[1]
+
+        start = self._adj_matrix.indptr[vertex]
+        end = self._adj_matrix.indptr[vertex + 1]
+        return adj_matrix.data[end] - adj_matrix.data[start]
     
-    # TODO: add functions to manage multiedges?
+    # TODO: add functions to manage multiedges
