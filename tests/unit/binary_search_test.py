eca76ff9973fa350f89b8d2533b56653f57ed522
CassiusThalles <cassiustcmendes@hotmail.com>
2024-02-26 00:11:01 -0300

    Refactor of recently created _binary_search
    First of all, it was neede to rename the function to _interval_binary_search due to its nature and also because the project would need the previous implementation of binary search. This _interval_binary_search logic is explained in the docstring

--- tests/unit/binary_search_test.py
+++ tests/unit/binary_search_test.py
@@ -1,35 +1,39 @@
 from sys import path as sys_path
 sys_path.append('../')
 sys_path.append('../../')
-from hiperwalk.refactor.graph import _binary_search
+from hiperwalk.refactor.graph import _interval_binary_search
 import unittest
 
 class TestBinarySearch(unittest.TestCase):
     def setUp(self):
-        self.sorted_array = [1, 1, 2, 3, 3, 4, 4, 5, 6, 6, 7, 8, 8, 9, 10]
-        self.unsorted_array = [1, 3, 10, 8, 8, 2, 5, 1, 9, 7, 6, 4, 6, 4, 3]
-    
-    def test_binary_search_element_in_the_array_unsorted(self):
-        with self.assertRaises(ValueError) as e:
-            _binary_search(self.unsorted_array, 4)
-        self.assertEqual(str(e.exception), "The array is not sorted.")
+        self.sorted_array = [1, 1, 2, 3, 3, 4, 4, 7, 8, 8, 9, 10]
+        self.sorted_array2 = [1, 2, 2, 3, 3, 4, 4, 7, 8, 8, 9, 10]
     
     def test_binary_search_element_less_than_the_minimum_value_in_the_array(self):
         with self.assertRaises(ValueError) as e:
-            _binary_search(self.sorted_array, 0)
+            _interval_binary_search(self.sorted_array, 0)
         self.assertEqual(str(e.exception), "The element is less than the minimum value in the array.")
     
     def test_binary_search_first_element_in_the_array(self):
-        # the result must be 0
-        self.assertEqual(_binary_search(self.sorted_array, 1), 0)
+        self.assertEqual(_interval_binary_search(self.sorted_array, 1), 1)
+    
+    def test_binary_search_first_element_in_the_array2(self):
+        self.assertEqual(_interval_binary_search(self.sorted_array2, 1), 0)
     
     def test_binary_search_element_greater_than_the_maximum_value_in_the_array(self):
-        #the result must be the last index of the array
-        self.assertEqual(_binary_search(self.sorted_array, 11), 14)
+        self.assertEqual(_interval_binary_search(self.sorted_array, 11), 11)
     
     def test_binary_search_element_in_the_middle_of_the_array(self):
-        # the binary search can receive a value between two indexes and the result must be the minimum of these two indexes
-        self.assertEqual(_binary_search(self.sorted_array, 4), 5)
+        self.assertEqual(_interval_binary_search(self.sorted_array, 4), 6)
     
-    def test_binary_search_element_greater_than_the_maximum_value_in_the_array(self):
-        self.assertEqual(_binary_search(self.sorted_array, 20), 14)
+    def test_binary_search_element_in_the_middle_of_the_array2(self):
+        self.assertEqual(_interval_binary_search(self.sorted_array2, 5), 6)
+    
+    def test_binary_search_element_in_the_middle_of_the_array3(self):
+        self.assertEqual(_interval_binary_search(self.sorted_array2, 6), 6)
+    
+    def test_binary_search_element_in_the_middle_of_the_array4(self):
+        self.assertEqual(_interval_binary_search(self.sorted_array2, 7), 7)
+    
+    def test_binary_search_element_in_the_middle_of_the_array5(self):
+        self.assertEqual(_interval_binary_search(self.sorted_array, 8), 9)
