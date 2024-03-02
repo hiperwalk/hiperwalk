de693b73bf5e2e46eb0e549d02b0c87bb66d7458
Koruja <gbezerra@posgrad.lncc.br>
2024-03-02 11:55:25 -0300

    Fix coined diagonal grid example

--- examples/coined-diagonal-grid.py
+++ examples/coined-diagonal-grid.py
@@ -3,12 +3,12 @@ import numpy as np
 
 dim = 121
 grid = hpw.Grid(dim, diagonal=True)
-center = np.array([dim // 2, dim // 2])
 dtqw = hpw.Coined(grid, shift='persistent', coin='grover')
-psi0 = dtqw.state([0.5, (center, center + (1, 1))],
-                  [-0.5, (center, center + (1, -1))],
-                  [-0.5, (center, center + (-1, 1))],
-                  [0.5, (center, center + (-1, -1))])
+center = np.array([dim // 2, dim // 2])
+psi0 = dtqw.state([[0.5, (center, center + (1, 1))],
+                   [-0.5, (center, center + (1, -1))],
+                   [-0.5, (center, center + (-1, 1))],
+                   [0.5, (center, center + (-1, -1))]])
 psi_final = dtqw.simulate(time=dim // 2, state=psi0, hpc=False)
 prob = dtqw.probability_distribution(psi_final)
 hpw.plot_probability_distribution(prob, graph=grid)
