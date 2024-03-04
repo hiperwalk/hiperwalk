d12232d499a2db12e78dbc77c1824d28c47d29b1
Koruja <gbezerra@posgrad.lncc.br>
2024-03-04 18:32:53 -0300

    Refactor Cycle and Line

--- hiperwalk/graph/cycle.py
+++ hiperwalk/graph/cycle.py
@@ -1,8 +1,6 @@
-from scipy.sparse import csr_array
-from .graph import Graph
 from .square_lattice import SquareLattice
 
-class Cycle(Graph):
+def Cycle(num_vert, weights=None, multiedges=None):
     r"""
     Cycle graph.
 
@@ -29,77 +27,6 @@ class Cycle(Graph):
         :caption: Figure 1.
 
     """
-
-    def __init__(self, num_vert):
-        # Creating adjacency matrix
-        # Every vertex is adjacent to two vertices.
-        data = np.ones(2*num_vert, dtype=np.int8)
-        indptr = 2*np.arange(num_vert + 1)
-
-        indices = np.zeros(len(data))
-        right = 1
-        left = num_vert - 1
-        for i in range(num_vert):
-            indices[2*i] = right
-            indices[2*i + 1] = left
-            right = (right + 1) % num_vert
-            left = (left + 1) % num_vert
-
-        adj_matrix = csr_array((data, indices, indptr))
-        self._adj_matrix = adj_matrix
-    
-        # initializing
-        #super().__init__(adj_matrix)
-
-    def arc_number(self, arc):
-        if not hasattr(arc, '__iter__'):
-            return super().arc_number(arc)
-
-        tail, head = arc
-        num_vert = self.number_of_vertices()
-        arc = (2*tail if (head - tail == 1
-                          or tail - head == num_vert - 1)
-               else 2*tail + 1)
-        return arc
-
-    def arc(self, number):
-        tail = number//2
-        remainder = number % 2
-        head = (tail + (-1)**remainder) % self.number_of_vertices()
-        return (tail, head)
-
-    def next_arc(self, arc):
-        try:
-            tail, head = arc
-            num_vert = self.number_of_vertices()
-            if (head - tail) % num_vert == 1:
-                # clockwise
-                return ((tail + 1) % num_vert, (head + 1) % num_vert)
-            elif (tail - head) % num_vert == 1:
-                # anticlockwise
-                return ((tail - 1) % num_vert, (head - 1) % num_vert)
-            else:
-                raise ValueError('Invalid arc.')
-        except TypeError:
-            # could not unpack. Arc label passed
-            num_arcs = self.number_of_arcs()
-            return ((arc + 2) % num_arcs if arc % 2 == 0
-                    else (arc - 2) % num_arcs)
-
-    def previous_arc(self, arc):
-        try:
-            tail, head = arc
-            num_vert = self.number_of_vertices()
-            if (head - tail) % num_vert == 1:
-                # previous clockwise
-                return ((tail - 1) % num_vert, (head - 1) % num_vert)
-            elif (tail - head) % num_vert == 1:
-                # previous anticlockwise
-                return ((tail + 1) % num_vert, (head + 1) % num_vert)
-            else:
-                raise ValueError('Invalid arc.')
-        except TypeError:
-            # could not unpack. Arc label passed
-            num_arcs = self.number_of_arcs()
-            return ((arc - 2) % num_arcs if arc % 2 == 0
-                    else (arc + 2) % num_arcs)
+    basis = [1, -1]
+    g = SquareLattice(num_vert, basis, True, weights, multiedges)
+    return g
