8f84d689e73589fb08f3f46f97266af0951e6092
Koruja <gbezerra@posgrad.lncc.br>
2024-05-23 22:16:13 -0300

    Fix complete bipartite graph constructor for multigraphs and weighted graphs

--- hiperwalk/graph/complete_bipartite.py
+++ hiperwalk/graph/complete_bipartite.py
@@ -1,7 +1,9 @@
 import numpy as np
 from .graph import Graph
 from types import MethodType
-from scipy.sparse import eye
+from scipy.sparse import eye, bmat, csr_array
+from .multigraph import Multigraph
+from .weighted_graph import WeightedGraph
 
 def adjacent(self, u, v):
     if u >= self._num_vert or v >= self._num_vert:
@@ -66,12 +68,12 @@ def degree(self, vertex):
 def adjacency_matrix(self):
     # it is more efficient to store the dense adj matrix than
     # the sparse one when explicit values are used
-    A = np.zeros((self._num_vert1, self._num_vert1), dtype=np.int8)
-    B = np.ones((self._num_vert1, self._num_vert2), dtype=np.int8)
-    C = np.ones((self._num_vert2, self._num_vert1), dtype=np.int8)
-    D = np.zeros((self._num_vert2, self._num_vert2), dtype=np.int8)
-    return np.block([[A, B],
-                     [C, D]])
+    # A = np.zeros((self._num_vert1, self._num_vert1), dtype=np.int8)
+    B = np.ones((self._num_vert1, self._num_vert2), dtype=np.int64)
+    C = np.ones((self._num_vert2, self._num_vert1), dtype=np.int64)
+    # D = np.zeros((self._num_vert2, self._num_vert2), dtype=np.int8)
+
+    return csr_array(bmat([[None, B], [C, None]]))
 
 def laplacian_matrix(self):
     A = self._num_vert2 * np.eye(self._num_vert1, dtype=np.int32)
@@ -81,7 +83,8 @@ def laplacian_matrix(self):
     return np.block([[A, B],
                      [C, D]])
 
-def CompleteBipartite(num_vert1, num_vert2, multiedges=None, weights=None):
+def CompleteBipartite(num_vert1, num_vert2, multiedges=None,
+                      weights=None, copy=False):
     r"""
     Complete bipartite graph constructor.
 
@@ -120,34 +123,59 @@ def CompleteBipartite(num_vert1, num_vert2, multiedges=None, weights=None):
     and the vertices in the second part are labeled from `num_vert1` 
     to `num_vert1 + num_vert2 - 1`.
     """
-    if weights is not None or multiedges is not None:
-        raise NotImplementedError()
+    if weights is not None and multiedges is not None:
+        raise ValueError(
+            "Both `weights` and `multiedges` arguments were set. "
+            + "Cannot decide whether to create a weighted graph or "
+            + "a multigraph."
+        )
 
     if num_vert1 <= 0 or num_vert2 <= 0:
         raise ValueError("There must be at least one vertex "
                          + "in each part.")
 
-    # toy graph
+    # simples graph
     g = Graph(eye(num_vert1 + num_vert2).tocsr())
-
-    # changes attributes
     del g._adj_matrix
     g._adj_matrix = None
     g._num_loops = 0
-
     g._num_vert1 = int(num_vert1)
     g._num_vert2 = int(num_vert2)
     g._num_vert = g._num_vert1 + g._num_vert2
-
-    g.adjacent = MethodType(adjacent, g)
+    g.adjacency_matrix = MethodType(adjacency_matrix, g)
+    g.laplacian_matrix = MethodType(laplacian_matrix, g)
     g._entry = MethodType(_entry, g)
     g._find_entry = MethodType(_find_entry, g)
+    g.number_of_edges = MethodType(number_of_edges, g)
+    g.degree = MethodType(degree, g)
+
+    data = weights if weights is not None else multiedges
+    if data is not None:
+        g._adj_matrix = g.adjacency_matrix()
+
+        if hasattr(data, 'keys'):
+            data = g._dict_to_adj_matrix(data)
+            copy = False
+        else:
+            g._rearrange_matrix_indices(data)
+
+        del g
+
+        if weights is not None:
+            g = WeightedGraph(data, copy=copy)
+            g.number_of_edges = MethodType(number_of_edges, g)
+            g.degree = MethodType(degree, g)
+        elif multiedges is not None:
+            g = Multigraph(data, copy=copy)
+
+        g._num_loops = 0
+        g._num_vert1 = int(num_vert1)
+        g._num_vert2 = int(num_vert2)
+        g._num_vert = g._num_vert1 + g._num_vert2
+
+    g.adjacent = MethodType(adjacent, g)
     g._neighbor_index = MethodType(_neighbor_index, g)
     g.neighbors = MethodType(neighbors, g)
     g.number_of_vertices = MethodType(number_of_vertices, g)
-    g.number_of_edges = MethodType(number_of_edges, g)
-    g.degree = MethodType(degree, g)
-    g.adjacency_matrix = MethodType(adjacency_matrix, g)
-    g.laplacian_matrix = MethodType(laplacian_matrix, g)
 
     return g
--- tests/unit/complete_bipartite.py
+++ tests/unit/complete_bipartite.py
@@ -25,3 +25,83 @@ class TestCompleteBipartite(unittest.TestCase):
         L = D - self.g.adjacency_matrix()
         L2 = self.g.laplacian_matrix()
         self.assertTrue(np.all(L - L2 == 0))
+
+    def test_multigraph(self):
+        adj = self.g.adjacency_matrix()
+        self.assertTrue(id(adj) != id(self.g._adj_matrix))
+
+        adj[0, self.n1] = 2
+        adj[self.n1, 0] = 2
+        adj[0, self.n - 1] = 3
+        adj[self.n - 1, 0] = 3
+
+        mg = hpw.Multigraph(adj, copy=True)
+        self.assertTrue(id(adj) != id(mg._adj_matrix))
+        self.assertTrue(id(adj) != id(mg.adjacency_matrix()))
+        self.assertTrue(np.sum(adj - mg.adjacency_matrix()) == 0)
+
+        mh = hpw.CompleteBipartite(self.n1, self.n2,
+                                   multiedges=adj, copy=True)
+        self.assertTrue(isinstance(mh, hpw.Multigraph))
+        self.assertTrue(id(adj) != id(mh._adj_matrix))
+        self.assertTrue(id(adj) != id(mh.adjacency_matrix()))
+        self.assertTrue(np.sum(adj - mh.adjacency_matrix()) == 0)
+
+        d = {(0, self.n1): 2, (0, self.n - 1): 3}
+        mh = hpw.CompleteBipartite(self.n1, self.n2, multiedges=d)
+        self.assertTrue(id(adj) != id(mh._adj_matrix))
+        self.assertTrue(id(adj) != id(mh.adjacency_matrix()))
+        self.assertTrue(np.sum(adj - mh.adjacency_matrix()) == 0)
+
+        mg = hpw.Multigraph(adj, copy=False)
+        self.assertTrue(id(adj) == id(mg._adj_matrix))
+        self.assertTrue(id(adj) != id(mg.adjacency_matrix()))
+
+        mh = hpw.CompleteBipartite(self.n1, self.n2,
+                                   multiedges=adj, copy=False)
+        self.assertTrue(id(adj) == id(mh._adj_matrix))
+        self.assertTrue(id(adj) != id(mh.adjacency_matrix()))
+
+        self.assertTrue(np.sum(mg._adj_matrix - mh._adj_matrix) == 0)
+
+    def test_weighted_graph(self):
+        adj = self.g.adjacency_matrix()
+        self.assertTrue(id(adj) != id(self.g._adj_matrix))
+
+        adj = adj.astype(float)
+        adj[0, self.n1] = 0.2
+        adj[self.n1, 0] = 0.2
+        adj[0, self.n - 1] = 0.3
+        adj[self.n - 1, 0] = 0.3
+
+        wg = hpw.WeightedGraph(adj, copy=True)
+        self.assertTrue(id(adj) != id(wg._adj_matrix))
+        self.assertTrue(id(adj) != id(wg.adjacency_matrix(True)))
+        self.assertTrue(id(adj) != id(wg.adjacency_matrix(False)))
+        self.assertTrue(np.sum(adj - wg._adj_matrix) == 0)
+
+        wh = hpw.CompleteBipartite(self.n1, self.n2,
+                                   weights=adj, copy=True)
+        self.assertTrue(isinstance(wh, hpw.WeightedGraph))
+        self.assertTrue(id(adj) != id(wh._adj_matrix))
+        self.assertTrue(id(adj) != id(wh.adjacency_matrix(True)))
+        self.assertTrue(id(adj) != id(wh.adjacency_matrix(False)))
+        self.assertTrue(np.sum(adj - wh._adj_matrix) == 0)
+
+        d = {(0, self.n1): 0.2, (0, self.n - 1): 0.3}
+        wh = hpw.CompleteBipartite(self.n1, self.n2, weights=d)
+        self.assertTrue(id(adj) != id(wh._adj_matrix))
+        self.assertTrue(id(adj) != id(wh.adjacency_matrix()))
+        self.assertTrue(np.sum(adj - wh._adj_matrix) == 0)
+
+        wg = hpw.WeightedGraph(adj, copy=False)
+        self.assertTrue(id(adj) == id(wg._adj_matrix))
+        self.assertTrue(id(adj) != id(wg.adjacency_matrix(True)))
+        self.assertTrue(id(adj) == id(wg.adjacency_matrix(False)))
+
+        wh = hpw.CompleteBipartite(self.n1, self.n2, weights=adj)
+        self.assertTrue(id(adj) == id(wh._adj_matrix))
+        self.assertTrue(id(adj) != id(wh.adjacency_matrix(True)))
+        self.assertTrue(id(adj) == id(wh.adjacency_matrix(False)))
+
+        self.assertTrue(np.sum(wg._adj_matrix - wh._adj_matrix) == 0)
