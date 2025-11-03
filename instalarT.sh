b1d69c4b4e25b7efba7fe07f1a6f81a51ac2fd90
bidu <ebiduardo@gmail.com>
2025-11-03 17:18:32 -0300

    meu primeiro commit C!

--- instalarT.sh
+++ instalarT.sh
@@ -1,35 +1,14 @@
-relativeDir=../
+relativeDir=./
 export PYTHONUNBUFFERED=1
 export LD_LIBRARY_PATH=/home/bidu/hiperblas/lib:$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH=$HOME/hiperblas/lib:$LD_LIBRARY_PATH
 echo LD_LIBRARY_PATH=$LD_LIBRARY_PATH
-comando="(cd $relativeDir/hiperblas-core/;. ./instalarHB.sh)"; echo $comando;
-comando="(cd $relativeDir/pyhiperblas/; ./instalarPyHB.sh)"; echo $comando;
-comando="(cd $relativeDir/hiperwalk/; pip install -e .  --break-system-packages)"; echo $comando;
-comando="(cd $relativeDir/hiperwalk; OMP_NUM_THREADS=1 stdbuf -oL time python3 -u examples/coined/diagonal-grid.py 2>&1) " ; echo $comando
-export OMP_NUM_THREADS=4
-read -p "aguardando um ok para continuar"
-comando="(cd $relativeDir/hiperblas-core/;. ./instalarHB.sh)"
-echo $comando;
-read -p "aguardando um ok para continuar"
-eval $comando
-comando="python3 -c 'import importlib.util; print(\"hiperblas ->\", importlib.util.find_spec(\"hiperblas\"))'"
-echo $comando;
-read -p "aguardando um ok para continuar"
-eval $comando
-pwd
-comando="(cd $relativeDir/pyhiperblas/; ./instalarPyHB.sh)"
-echo $comando;
-read -p "aguardando um ok para continuar"
-eval $comando
-comando="(cd $relativeDir/hiperwalk/; pip install -e .  --break-system-packages)"
-echo $comando;
-read -p "aguardando um ok para continuar"
-eval $comando
-comando="(cd $relativeDir/hiperwalk; OMP_NUM_THREADS=1 stdbuf -oL time python3 -u examples/coined/diagonal-grid.py 2>&1)  |grep -v \"BD\""
-comando="(cd $relativeDir/hiperwalk; OMP_NUM_THREADS=1 stdbuf -oL time python3 -u examples/coined/diagonal-grid.py 2>&1) " 
-echo $comando;
-read -p "aguardando um ok para continuar"
-eval $comando
-comando="(cd $relativeDir/hiperwalk; python3 examples/coined/hypercube.py |grep -v \"em mat\|-core\" ) "
-#echo $comando; eval $comando
+comandoA="(cd $relativeDir/hiperblas-core/;. ./instalarHB.sh)"; echo $comando;
+eval $comandoA
+comandoB="(cd $relativeDir/pyhiperblas/; ./instalarPyHB.sh $PWD)"; echo $comando;
+eval $comandoB
+comandoC="(cd $relativeDir; pip install -e .  --break-system-packages)"; echo $comando;
+eval $comandoC
+comandoD="(cd $relativeDir; OMP_NUM_THREADS=1 stdbuf -oL time python3 -u examples/coined/diagonal-grid.py 2>&1) " ; echo $comando
+comandoD="(cd $relativeDir; OMP_NUM_THREADS=1 stdbuf -oL time python3 -u examples/coined/hiperblasCPLX.py 2>&1) " ; echo $comando
+eval $comandoD
