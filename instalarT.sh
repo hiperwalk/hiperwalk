309668857bd8dd2ec0d7c829e4a196f54148897c
bidu <ebiduardo@gmail.com>
2025-11-03 18:43:22 -0300

    meu primeiro commit E!

--- instalarT.sh
+++ instalarT.sh
@@ -10,5 +10,5 @@ eval $comandoB
 comandoC="(cd $relativeDir; pip install -e .  --break-system-packages)"; echo $comando;
 eval $comandoC
 comandoD="(cd $relativeDir; OMP_NUM_THREADS=1 stdbuf -oL time python3 -u examples/coined/diagonal-grid.py 2>&1) " ; echo $comando
-comandoD="(cd $relativeDir; OMP_NUM_THREADS=1 stdbuf -oL time python3 -u examples/coined/hiperblasCPLX.py 2>&1) " ; echo $comando
+comandoD="(cd $relativeDir; OMP_NUM_THREADS=1 stdbuf -oL time python3 -u examples/coined/hyperblasCPLX.py 2>&1) " ; echo $comando
 eval $comandoD
